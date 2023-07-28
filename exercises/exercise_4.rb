a3a248efcbd133388c0c3fa51861eeb9dd1b2db0
adavidliang <adavidliang@gmail.com>
2023-07-28 13:18:23 -0400

    create 3 more table

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,20 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true)
+
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true)
+
+Store.create(name: "Gastown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+pp @mens_stores = Store.where(mens_apparel: true)
+
+Store.select {|stores|
+pp stores.name
+pp stores.annual_revenue
+}
+
+pp Store.select{|store| 
+store.annual_revenue < 1000000 && store.womens_apparel == true
+}
+
